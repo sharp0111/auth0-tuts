4bbb438b7d507c1396eee9467136724549da76c9
Res Yeg <shaqk702@gmail.com>
2018-10-22 10:42:13 -0700

    add basic reducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>We will create an awesome game with React, Redux, and SVG!</h1>
+        <h1>{this.props.message}</h1>
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  message: PropTypes.String.isRequired,
+}
+
+export default App
