8a6b5d26bcbc02015369f81752c9737c89021598
Res Yeg <shaqk702@gmail.com>
2018-10-23 18:55:06 -0700

    add Canvas component

---
+++ src/components/Canvas.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Canvas = () => {
+  const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
+  return (
+    <svg
+      id="aliens-go-home-canvas"
+      preserveAspectRatio="xMaxYMax none"
+      viewBox={viewBox}
+    >
+      <circle cx={0} cy={0} r={50} />
+    </svg>
+  );
+};
+
+export default Canvas;
+
+// import React from 'react';
+
+// const Canvas = () => {
+//   const style = {
+//     border: '1px solid black',
+//   };
+//   return (
+//     <svg
+//       id="aliens-go-home-canvas"
+//       preserveAspectRatio="xMaxYMax none"
+//       style={style}
+//     >
+//       <circle cx={0} cy={0} r={50} />
+//     </svg>
+//   );
+// };
+
+// export default Canvas;
