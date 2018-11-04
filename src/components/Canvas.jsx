8f9458a3d34974dd4b2d50c58138a43533b94632
Res Yeg <shaqk702@gmail.com>
2018-11-04 10:02:00 -0800

    make Cannon aim

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,43 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Sky from './Sky';
 import Ground from './Ground';
 import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
 
-const Canvas = () => {
+const Canvas = (props) => {
   const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
   return (
     <svg
       id="aliens-go-home-canvas"
       preserveAspectRatio="xMaxYMax none"
+      onMouseMove={props.trackMouse}
       viewBox={viewBox}
     >
       <Sky />
       <Ground />
-      <CannonPipe rotation={45} />
+      <CannonPipe rotation={props.angle} />
       <CannonBase />
       {/* <circle cx={0} cy={0} r={50} /> */}
     </svg>
   );
 };
 
+Canvas.propTypes = {
+  angle: PropTypes.number.isRequired,
+  trackMouse: PropTypes.func.isRequired,
+};
+
 export default Canvas;
-
-// import React from 'react';
-
-// const Canvas = () => {
-//   const style = {
-//     border: '1px solid black',
-//   };
-//   return (
-//     <svg
-//       id="aliens-go-home-canvas"
-//       preserveAspectRatio="xMaxYMax none"
-//       style={style}
-//     >
-//       <circle cx={0} cy={0} r={50} />
-//     </svg>
-//   );
-// };
-
-// export default Canvas;
