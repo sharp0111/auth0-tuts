f45fc3d83a2b08e31be61e0042e40b6b217fe5b1
Res Yeg <shaqk702@gmail.com>
2018-11-10 19:44:28 -0800

    add routes for Home, Airport, City components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
+
+const Home = () => (
+  <div>
+    <h2> Home </h2>
+  </div>
+);
+
+const Airport = () => (
+  <div>
+    <ul>
+      <li>Jomo Kenyatta</li>
+      <li>Tambo</li>
+      <li>Muurtala Mohammed</li>
+    </ul>
+  </div>
+)
+
+const City = () => (
+  <div>
+    <ul>
+      <li>San Francisco</li>
+      <li>Istanbul</li>
+      <li>Tokyo</li>
+    </ul>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="airports">Airport</Link></li>
+          <li><Link to="/cities">Cities</Link></li>
+        </ul>
+
+        <Route path="/" exact component={Home} />
+        <Route path="/airports" component={Airport} />
+        <Route path="/cities" component={City} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -16,11 +16,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Router>
     <App />
   </Router>, document.getElementById('root'));
 
-registerServiceWorker();
+// registerServiceWorker();
