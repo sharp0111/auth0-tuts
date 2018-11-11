8010ccbed433c833fd82b5f072faef288f32f9ac
Res Yeg <shaqk702@gmail.com>
2018-11-10 19:58:40 -0800

    finish Nested Routing and URL Parameters

--- src/App.js
+++ src/App.js
@@ -29,6 +29,21 @@ const City = () => (
   </div>
 )
 
+const Courses = ({ match }) => (
+  <div>
+     <ul>
+        <li><Link to={`${match.url}/technology`}>Technology</Link></li>
+        <li><Link to={`${match.url}/business`}>Business</Link></li>
+        <li><Link to={`${match.url}/economics`}>Economics</Link></li>
+    </ul>
+
+    {/* <Route exact path={`${match.path}/technology`} render={() => (<div> This is technology </div>)}/>
+    <Route path={`${match.path}/business`} component={() => (<div> This is business </div>)}/>
+    <Route path={`${match.path}/economics`} component={() => (<div> This is economics </div>)}/> */}
+    <Route exact path={`${match.path}/:course`} render={({match}) => (<div> This is {match.params.course} </div>)}/>
+  </div>
+);
+
 class App extends Component {
   render() {
     return (
@@ -37,11 +52,14 @@ class App extends Component {
           <li><Link to="/">Home</Link></li>
           <li><Link to="airports">Airport</Link></li>
           <li><Link to="/cities">Cities</Link></li>
+          <li><Link to="/courses">Courses</Link></li>
+          <li><Link to="/cities">Cities</Link></li>
         </ul>
 
         <Route path="/" exact component={Home} />
         <Route path="/airports" component={Airport} />
         <Route path="/cities" component={City} />
+        <Route path="/courses" component={Courses}/>
       </div>
     );
   }
