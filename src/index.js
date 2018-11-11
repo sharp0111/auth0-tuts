f3a78f37ea2b9d5fa4ee9ff86585af83ce958251
Res Yeg <shaqk702@gmail.com>
2018-11-10 17:34:52 -0800

    finish migration from redux to context

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import reducers from './reducers'
+// import { Provider } from 'react-redux'
+// import { createStore } from 'redux'
+// import reducers from './reducers'
+import Provider from './providers';
+import Consumer from './consumer';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 // Creating the store using the reducers info.
 // That's because reducers are the building blocks of a Redux Store.
-const store = createStore(reducers)
+// const store = createStore(reducers)
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
+    // <Provider store={store}>
+    //     <App />
+    // </Provider>
+    <Provider>
+        <Consumer>
+            <App />
+        </Consumer>
     </Provider>
     , document.getElementById('root'));
 
