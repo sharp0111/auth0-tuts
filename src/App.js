e4d27b5e45a70b3f407fdc63d6b2de7c63b66fba
Res Yeg <shaqk702@gmail.com>
2018-11-10 20:22:46 -0800

    finish Route Protection and Authentication

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-import { Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Redirect, withRouter } from 'react-router-dom';
 import './App.css';
 
+// Basic Routing
 const Home = () => (
   <div>
     <h2> Home </h2>
@@ -29,6 +30,7 @@ const City = () => (
   </div>
 )
 
+// Nested Routing & URL Parameters
 const Courses = ({ match }) => (
   <div>
      <ul>
@@ -43,6 +45,81 @@ const Courses = ({ match }) => (
     <Route exact path={`${match.path}/:course`} render={({match}) => (<div> This is {match.params.course} </div>)}/>
   </div>
 );
+
+// Route Protection and Authentication
+const Public = () => (
+  <div> This is a public page </div>
+);
+
+const Private = () => (
+  <div> This is a private page </div>
+);
+
+// const Login = () => (
+//   <div> Login Page <button>login</button> </div>
+// );
+class Login extends React.Component {
+  state = {
+    redirectToPreviousRoute: false
+  };
+
+  login = () => {
+    AuthService.authenticate(() => {
+      this.setState({ redirectToPreviousRoute: true });
+    });
+  };
+
+  render() {
+    const { from } = this.props.location.state || { from: { pathname: "/" } };
+    const { redirectToPreviousRoute } = this.state;
+
+    if (redirectToPreviousRoute) {
+      return <Redirect to={from} />;
+    }
+
+    return (
+      <div>
+        <p>You must log in to view the page at {from.pathname}</p>
+        <button onClick={this.login}>Log in</button>
+      </div>
+    );
+  }
+}
+
+const AuthService = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true
+    setTimeout(cb, 100)
+  },
+  logout(cb) {
+    this.isAuthenticated = false
+    setTimeout(cb, 100)
+  }
+};
+
+const SecretRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props) => (
+    AuthService.isAuthenticated === true
+      ? <Component {...props} />
+      : <Redirect to={{
+        pathname: '/login',
+        state: { from: props.location }
+      }} />
+  )} />
+);
+
+const AuthStatus = withRouter(({ history }) => (
+  AuthService.isAuthenticated ? (
+    <p>
+      Welcome! <button onClick={() => {
+        AuthService.logout(() => history.push('/'))
+      }}>Sign out</button>
+    </p>
+  ) : (
+    <p>You are not logged in.</p>
+  )
+));
 
 class App extends Component {
   render() {
@@ -60,7 +137,26 @@ class App extends Component {
         <Route path="/airports" component={Airport} />
         <Route path="/cities" component={City} />
         <Route path="/courses" component={Courses}/>
+
+        <Router>
+          <div style={{width: 1000, margin: '0 auto'}}>
+            <AuthStatus />
+            <ul>
+              <li><Link to='/public'> Public </Link></li>
+              <li><Link to='/private'> Private </Link></li>
+            </ul>
+
+            <hr/>
+
+            <Route path='/public' component={Public} />
+            <Route path="/login" component={Login}/>
+            {/* <Route path='/private' component={Private} /> */}
+            <SecretRoute path='/private' component={Private} />
+
+          </div>
+        </Router>
       </div>
+
     );
   }
 }
