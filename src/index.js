379586e400ac6c74079f4e80afd627645029d29f
Res Yeg <shaqk702@gmail.com>
2018-11-10 16:41:57 -0800

    add redux store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from './reducers'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Creating the store using the reducers info.
+// That's because reducers are the building blocks of a Redux Store.
+const store = createStore(reducers)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
